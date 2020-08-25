90a29eb40905939c64ed39d26cadd64ead5f248b
jmckiern <jmckiern@tcd.ie>
2020-08-25 22:00:05 +0100

    Add --upgrade flag

--- update_dependencies.py
+++ update_dependencies.py
@@ -4,4 +4,4 @@ import sys
 
 dependencies = ['selenium', 'requests']
 
-subprocess.check_call([sys.executable, *(' '.join(['-m pip install', *dependencies, '--ignore-installed -t 3rdparty/']).split(' '))])
+subprocess.check_call([sys.executable, *(' '.join(['-m pip install', *dependencies, '--upgrade --ignore-installed -t 3rdparty/']).split(' '))])
