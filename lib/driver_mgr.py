ac9fa1934687a1a95827d0f71f8b009d9455ba8e
jmckiern <jmckiern@tcd.ie>
2020-09-07 22:20:12 +0100

    Add workaround for geckodriver bug

--- lib/driver_mgr.py
+++ lib/driver_mgr.py
@@ -26,6 +26,7 @@ def CreateDriver(path: str):
     elif driver_name == "geckodriver":
         options = webdriver.FirefoxOptions()
         options.headless = True
+        options.accept_insecure_certs = False
         log_dir = os.path.abspath(os.path.dirname(__file__) + '/../log')
         if not os.path.exists(log_dir):
             os.mkdir(log_dir)
